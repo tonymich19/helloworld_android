5df69eb2d34b89a12de0d4787d214571fbe3f239
tonymic <tony.correia19@outlook.com>
2023-10-26 22:33:06 -0300

    introduction to androix

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
@@ -0,0 +1,11 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.orgs.R
+
+class ProductFormActivity : AppCompatActivity(
+    R.layout.activity_product_form
+) {
+
+}
